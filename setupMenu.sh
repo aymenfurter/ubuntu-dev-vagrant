3e9ff08657129efae489c5ffe4b31e83bd830af4
Aymen Furter <aymen.furter@gmail.com>
2019-07-28 19:45:09 +0200

    Create setupMenu.sh

---
+++ setupMenu.sh
@@ -0,0 +1,3 @@
+dconf write /org/gnome/shell/favorite-apps "['code.desktop','org.gnome.Nautilus.desktop','firefox.desktop', 'eclipse.desktop','soapui.desktop','terminal.desktop']"
+notify-send "Setup Complete!"
+rm ~/Desktop/complete-setup.desktop
